f76f42d9cf8e912c3c1145e9adf3a42a40fe2de1
arloor <admin@arloor.com>
2018-08-08 21:37:34 +0800

    小改

--- src/LocalSelector.java
+++ src/LocalSelector.java
@@ -8,12 +8,13 @@ import java.nio.channels.*;
 import java.util.Iterator;
 import java.util.Set;
 
-public class LocalSelector implements Runnable{
-    private static Logger logger=LoggerFactory.getLogger(LocalSelector.class);
+public class LocalSelector implements Runnable {
+    private static Logger logger = LoggerFactory.getLogger(LocalSelector.class);
     private Selector selectorLocal;
     private ServerSocketChannel acceptChannel;
-    private final static LocalSelector instance=new LocalSelector();
-    public static LocalSelector getInstance(){
+    private final static LocalSelector instance = new LocalSelector();
+
+    public static LocalSelector getInstance() {
         return instance;
     }
 
@@ -22,7 +23,7 @@ public class LocalSelector implements Runnable{
         try {
             this.selectorLocal = Selector.open();
             acceptChannel = ServerSocketChannel.open();
-            acceptChannel.bind(new InetSocketAddress(InetAddress.getLoopbackAddress(),8080));
+            acceptChannel.bind(new InetSocketAddress(InetAddress.getLoopbackAddress(), 8080));
             acceptChannel.configureBlocking(false);
             acceptChannel.register(selectorLocal, SelectionKey.OP_ACCEPT);
         } catch (IOException e) {
@@ -30,40 +31,42 @@ public class LocalSelector implements Runnable{
         }
     }
 
-    public void listen(){
-        try {
-            while(true){
-                //循环selctor
-                int numKey= selectorLocal.select();
-                if(numKey>0){
-                    Set<SelectionKey> keys= selectorLocal.selectedKeys();
-                    Iterator<SelectionKey> keyIterator=keys.iterator();
-                    while(keyIterator.hasNext()){
-                        SelectionKey key=keyIterator.next();
-                        if(key.isAcceptable()) {//proxyChannel接收来自浏览器的连接
+    public void listen() {
+
+        while (true) {
+            //循环selctor
+            try {
+                int numKey = selectorLocal.select();
+                if (numKey > 0) {
+                    Set<SelectionKey> keys = selectorLocal.selectedKeys();
+                    Iterator<SelectionKey> keyIterator = keys.iterator();
+                    while (keyIterator.hasNext()) {
+                        SelectionKey key = keyIterator.next();
+                        if (key.isAcceptable()) {//proxyChannel接收来自浏览器的连接
                             accept();
-                        }else if(key.isReadable()){//读来自浏览器的请求
-                            ChannalBridge channalBridge=(ChannalBridge) key.attachment();
+                        } else if (key.isReadable()) {//读来自浏览器的请求
+                            ChannalBridge channalBridge = (ChannalBridge) key.attachment();
                             channalBridge.readLocal();
                         }
                         keyIterator.remove();
                     }
                 }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
+
     }
 
 
-    public void accept(){
+    public void accept() {
         try {
             SocketChannel localChanel = acceptChannel.accept();
             localChanel.configureBlocking(false);
             logger.info("接收浏览器连接: " + localChanel.getRemoteAddress());
-            ChannalBridge channalBridge=new ChannalBridge(localChanel);
+            ChannalBridge channalBridge = new ChannalBridge(localChanel);
 //            channalBridgeHub.add(channalBridge);
-            this.listenLocalChannel(localChanel,channalBridge);
+            this.listenLocalChannel(localChanel, channalBridge);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -71,7 +74,7 @@ public class LocalSelector implements Runnable{
 
     private void listenLocalChannel(SocketChannel localChanel, ChannalBridge channalBridge) {
         try {
-            localChanel.register(selectorLocal,SelectionKey.OP_READ,channalBridge);
+            localChanel.register(selectorLocal, SelectionKey.OP_READ, channalBridge);
             channalBridge.setLocalSelectionKey(localChanel.keyFor(selectorLocal));
         } catch (ClosedChannelException e) {
             e.printStackTrace();
--- src/RemoteSelector.java
+++ src/RemoteSelector.java
@@ -12,21 +12,21 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 
 
 public class RemoteSelector implements Runnable {
-    private static Logger logger=LoggerFactory.getLogger(RemoteSelector.class);
+    private static Logger logger = LoggerFactory.getLogger(RemoteSelector.class);
     private Selector selectorRemote;
-    private final static RemoteSelector instance=new RemoteSelector();
+    private final static RemoteSelector instance = new RemoteSelector();
     //这个队列用于保存在LocalSelector线程中创建的到远程服务器的channel
     //为什么要这样？因为localSelector和remoteSelector是两个线程。
     //而remoteSeletor在select是会阻塞。阻塞时占有锁（在源码中是所在了publicKeys）。
     //而channel向selector注册时，也需要占有这个锁。最终结果就是register会被阻塞。
     //这就导致了，响应一直到不了。
-    private ConcurrentLinkedQueue<ChannalBridge> waitRegisterBridges=new ConcurrentLinkedQueue<>();
+    private ConcurrentLinkedQueue<ChannalBridge> waitRegisterBridges = new ConcurrentLinkedQueue<>();
 
-    public static RemoteSelector getInstance(){
+    public static RemoteSelector getInstance() {
         return instance;
     }
 
-    private  RemoteSelector() {
+    private RemoteSelector() {
         try {
             this.selectorRemote = Selector.open();
         } catch (IOException e) {
@@ -35,42 +35,44 @@ public class RemoteSelector implements Runnable {
     }
 
 
-    public void listenRemoteChannel(SocketChannel remoteChannal, ChannalBridge channalBridge){
+    public void listenRemoteChannel(SocketChannel remoteChannal, ChannalBridge channalBridge) {
         try {
-            remoteChannal.register(selectorRemote,SelectionKey.OP_READ,channalBridge);
+            remoteChannal.register(selectorRemote, SelectionKey.OP_READ, channalBridge);
             channalBridge.setRemoteSelectionKey(remoteChannal.keyFor(selectorRemote));
         } catch (ClosedChannelException e) {
             e.printStackTrace();
         }
     }
 
-    private void listen(){
-        try {
-            while(true){
+    private void listen() {
+
+        while (true) {
+            try {
                 //注册remoteChannal
                 ChannalBridge waitRegisterBridge;
-                while ((waitRegisterBridge=waitRegisterBridges.poll())!=null){
-                    logger.info("注册remoteChannel到remoteSelector。remoteChannel: "+waitRegisterBridge.getRemoteChannal().getRemoteAddress());
-                    this.listenRemoteChannel(waitRegisterBridge.getRemoteChannal(),waitRegisterBridge);
+                while ((waitRegisterBridge = waitRegisterBridges.poll()) != null) {
+                    logger.info("注册remoteChannel到remoteSelector。remoteChannel: " + waitRegisterBridge.getRemoteChannal().getRemoteAddress());
+                    this.listenRemoteChannel(waitRegisterBridge.getRemoteChannal(), waitRegisterBridge);
                 }
                 //循环selctor
-                int numKey= selectorRemote.select();
-                if(numKey>0){
-                    Set<SelectionKey> keys= selectorRemote.selectedKeys();
-                    Iterator<SelectionKey> keyIterator=keys.iterator();
-                    while(keyIterator.hasNext()){
-                        SelectionKey key=keyIterator.next();
-                        if(key.isReadable()){//读来自远程服务器的响应
-                            ChannalBridge channalBridge=(ChannalBridge) key.attachment();
+                int numKey = selectorRemote.select();
+                if (numKey > 0) {
+                    Set<SelectionKey> keys = selectorRemote.selectedKeys();
+                    Iterator<SelectionKey> keyIterator = keys.iterator();
+                    while (keyIterator.hasNext()) {
+                        SelectionKey key = keyIterator.next();
+                        if (key.isReadable()) {//读来自远程服务器的响应
+                            ChannalBridge channalBridge = (ChannalBridge) key.attachment();
                             channalBridge.readRemoteAndSendToLocal();
                         }
                         keyIterator.remove();
                     }
                 }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
+
     }
 
     public void addWaitRegistBridge(ChannalBridge channalBridge) {
